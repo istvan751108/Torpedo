88a75ec9a8864ff2abb5d1475defad434f33511e
István Szabó <istvan751108@gmail.com>
2021-11-17 17:04:49 +0100

    Added GamerVO.java + Gamer greetings.

--- src/main/java/torpedo/Main.java
+++ src/main/java/torpedo/Main.java
@@ -1,4 +1,13 @@
 package torpedo;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add meg a játékos nevét:");
+        GamerVO gamerVO = new GamerVO(scanner.nextLine());
+        System.out.println("Kellemes szórakozást kívánok neked," + gamerVO.getNev() + "!");
+    }
 }
+
---
+++ src/main/java/torpedo/MapVO.java
@@ -0,0 +1,5 @@
+package torpedo;
+
+public class MapVO {
+
+}
