c0191e7736c154c2a1c62b63e738b01f47b7866f
István Szabó <istvan751108@gmail.com>
2022-01-06 18:57:45 +0100

    Added Unit test file

---
+++ src/test/java/torpedo/persistence/impl/JdbcPlayerRepositoryTest.java
@@ -0,0 +1,61 @@
+package torpedo.persistence.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import torpedo.model.GamerVO;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class JdbcPlayerRepositoryTest {
+
+    private JdbcPlayerRepository jdbcPlayerRepositoryUnderTest;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        jdbcPlayerRepositoryUnderTest = new JdbcPlayerRepository(new GamerVO("player1", 0));
+    }
+
+    @Test
+    void testAddPlayer() {
+        // Setup
+        final GamerVO gamerVO = new GamerVO("player1", 0);
+
+        // Run the test
+        jdbcPlayerRepositoryUnderTest.addPlayer(gamerVO);
+
+        // Verify the results
+    }
+
+    @Test
+    void testModPlayer() {
+        // Setup
+        final GamerVO gamerVO = new GamerVO("player1", 0);
+
+        // Run the test
+        jdbcPlayerRepositoryUnderTest.modPlayer(gamerVO);
+
+        // Verify the results
+    }
+
+    @Test
+    void testClose() throws Exception {
+        // Setup
+        // Run the test
+        jdbcPlayerRepositoryUnderTest.close();
+
+        // Verify the results
+    }
+
+    @Test
+    void testReadPlayer() {
+        // Setup
+        final GamerVO expectedResult = new GamerVO("player1", 0);
+
+        // Run the test
+        final GamerVO result = jdbcPlayerRepositoryUnderTest.readPlayer();
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+}
